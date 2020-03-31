48471376648e6e5d0d19a02989b95fc3949bbcc7
Oumou el mouminine dhmine <18067.dhmine@esp.mr>
2020-03-31 19:03:05 +0000

    Delete Etabli_PartenaireRepository.java

--- Etabli_PartenaireRepository.java
+++
@@ -1,9 +0,0 @@
-package mr.esp.springdemo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import mr.esp.springdemo.model.Etabli_Partenaire;
-
-public interface Etabli_PartenaireRepository extends CrudRepository<Etabli_Partenaire, Integer> {
-
-}
