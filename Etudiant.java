ac422998255ae967b9d765d333f7c30dbe26f8d9
Oumou el mouminine dhmine <18067.dhmine@esp.mr>
2020-03-31 21:13:34 +0000

    add Entity Etudiant

---
+++ Etudiant.java
@@ -0,0 +1,72 @@
+package mr.esp.springdemo.model;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+@Entity
+public class Etudiant {
+	@Id
+	private Integer matricule;
+	public Integer getMatricule() {
+		return matricule;
+	}
+	public void setMatricule(Integer matricule) {
+		this.matricule = matricule;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public Date getDateDeNaissence() {
+		return dateDeNaissence;
+	}
+	public void setDateDeNaissence(Date dateDeNaissence) {
+		this.dateDeNaissence = dateDeNaissence;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public String getNni() {
+		return nni;
+	}
+	public void setNni(String nni) {
+		this.nni = nni;
+	}
+	private String nom;
+	private String prenom;
+	 @Temporal(TemporalType.DATE)
+	    private Date dateDeNaissence;
+	 private String telephone;
+	 private String email;
+	    private String photo;
+	    private String nni;
+	
+
+}
