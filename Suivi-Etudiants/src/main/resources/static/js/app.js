ef49b15ace9ab0c50c5190e4aca2180c130cf163
Djeinaba Mamadou ba <18015.mamadou.aly@esp.mr>
2020-04-21 14:02:15 +0000

    mise à jour de la Première version du Formulaire de saisie des Ecoles Partenaires

--- Suivi-Etudiants/src/main/resources/static/js/app.js
+++ Suivi-Etudiants/src/main/resources/static/js/app.js
@@ -1,26 +1,17 @@
-<<<<<<< HEAD
 const apiURL = location.origin + '/Suivi-Etudiants/ecole';
 function getPatientJson() {
   var formData = {
     pays:$('#pays').val(),
     adresse:$('#adresse').val(),
-    email:$('#email').val()
-    nom:$('#nom').val()
-    quotas:$('#quotas').val()
-    site_web:$('#site_web').val()
-    telephone:$('#telephone').val()
+    email:$('#email').val(),
+    nom:$('#nom').val(),
+    quotas:$('#quotas').val(),
+    site_web:$('#site_web').val(),
+    telephone:$('#telephone').val(),
     type_accords:$('#type_accords').val(),
     ville:$('#ville').val(),
-    specialite:$('#specialite').val(),
+    specialite:$('#Specialite').val(),
     
-=======
-const apiURL = location.origin + '/esp-template-spring/student';
-function getPatientJson() {
-  var formData = {
-    name:$('#name').val(),
-    surname:$('#surname').val(),
-    dateOfBirth:$('#dateOfBirth').val()
->>>>>>> 5cce158688d76488fc9445f95a81aa9fbdbdc5af
   };
   return JSON.stringify(formData);
 }
@@ -46,7 +37,6 @@ $("#doCreateButton").on("click", function(event) {
     data : sJSON,
     contentType: "application/json",
     dataType: "json",
-<<<<<<< HEAD
     success : function(msg) {   
               $("#EcoleFormBanner").html('Data Submitted successfully');
               $("#EcoleFormBanner").attr('class', 'alert alert-success');
@@ -61,41 +51,9 @@ $("#doCreateButton").on("click", function(event) {
               console.log(error);
             }
          });
-      }
+      });
 
 $("#listEcoles").ready(function() {
-=======
-    success : function(msg) {
-      $("#studentFormBanner").html('Data Submitted successfully');
-      $("#studentFormBanner").attr('class', 'alert alert-success');
-      setTimeout(function() { $("#studentFormBanner").addClass('d-none'); }, 10000);
-      console.log(msg);
-    },
-    error : function (request, status, error) {
-      $("#studentFormBanner").html('Error!');
-      $("#studentFormBanner").attr('class', 'alert alert-danger');
-      setTimeout(function() { $("#studentFormBanner").addClass('d-none'); }, 10000);
-      console.log(request.responseText);
-      console.log(error);
-    }
-  });
-});
-
-function showStudents(data) {
-	var content = '';
-    for (var i = 0; i < data.length; i++) {
-	    content += '<tr id="' + data[i].id + '">';
-	    content += '<td>' + data[i].name + '</td>';
-	    content += '<td>' + data[i].surname + '</td>';
-	    content += '<td>' + data[i].dateOfBirth + '</td>';
-	    content += '<th scope="row"> <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteModal" data-studentid="' + data[i].id + '">&times;</button></th>';
-	    content += '</tr>';
-    }
-    $('#listEtudiants tbody').html(content);
-}
-
-$("#listEtudiants").ready(function() {
->>>>>>> 5cce158688d76488fc9445f95a81aa9fbdbdc5af
 	
 	$.ajax({
 		type : "GET",
@@ -103,7 +61,6 @@ $("#listEtudiants").ready(function() {
 		cache:false,
 		dataType: "json",
 		success : function(data) {
-<<<<<<< HEAD
 			console.log(data);
 			var content = '';
 		    for (var i = 0; i < data.length; i++) {
@@ -117,15 +74,11 @@ $("#listEtudiants").ready(function() {
 			    content += '<td>' + data[i].telephone + '</td>';
 			    content += '<td>' + data[i].type_accords + '</td>';
 			    content += '<td>' + data[i].ville + '</td>';
-			    content += '<td>' + data[i].specialite + '</td>';
-			    
+			    content += '<td>' + data[i].specialite + '</td>'; 
 			    content += '<th scope="row">' + data[i].id + '</th>';
 			    content += '</tr>';
 		    }
 		    $('#listEcoles tbody').html(content);
-=======
-			showStudents(data);
->>>>>>> 5cce158688d76488fc9445f95a81aa9fbdbdc5af
 		},
 		error : function (request, status, error) {
 		      console.log(request.responseText);
@@ -136,31 +89,3 @@ $("#listEtudiants").ready(function() {
 
 });
 
-<<<<<<< HEAD
-=======
-$('#deleteModal').on('show.bs.modal', function (event) {
-	var button = $(event.relatedTarget) // Button that triggered the modal
-	var studentid = button.data('studentid') 
-	var modal = $(this)
-	modal.find('.modal-title').text('Supprimer Etudiant ' + studentid)
-	modal.find('#doDeleteButton').data('studentid', studentid)
-});
-
-$("#doDeleteButton").on("click", function(event) {
-	event.preventDefault();
-	var studentid = $(this).data('studentid')
-	$.ajax({
-		type : "DELETE",
-		url : apiURL + '/' + studentid,
-		success : function(msg) {
-			console.log("deleted " + studentid);
-			$("#"+studentid).hide();
-		},
-		error : function (request, status, error) {
-			console.log(request.responseText);
-		    console.log(error);
-		}
-	});
-});
-
->>>>>>> 5cce158688d76488fc9445f95a81aa9fbdbdc5af
